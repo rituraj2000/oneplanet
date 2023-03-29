d1ff180a36175db155dfd826e794ec9d84ff6891
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2023-03-29 13:48:48 +0530

    feat: event cards

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 /// Contains all the colors used in the app.
 class AppColors {
   static const Color kGreenYellow = Color(0xffC2CB0F);
+  static const Color kCardTagGreen = Color(0xffa6d457);
   static const Color kCardLightGreen = Color(0xffd1eca0);
   static const Color kCardLightYellow = Color(0xff97f1cf);
   static const Color kCardTextGreen = Color(0xff38D996);
